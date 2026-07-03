faa9bab8c4b5ae391c235b3d3fc10b2556e0caff
anya-popova <anya.anyapopova.popova@mail.ru>
2026-07-04 01:26:24 +0300

    HW3

---
+++ tests/FAQ.spec.ts
@@ -0,0 +1,8 @@
+import {test, expect} from '@playwright/test';
+
+test('Раскрытый бургер ответа на вопрос на старнице FAQ', async ({ page }) => {
+
+    await page.goto('/faq');
+    await page.getByTestId('faq-question-1').click();
+    await expect.soft(page).toHaveScreenshot();
+});
--- tests/shoppingCart.spec.ts
+++ tests/shoppingCart.spec.ts
@@ -1,10 +1,25 @@
 import {test, expect} from '@playwright/test';
 
-test ('Корзина с товаром', async ({page}) => {
+test('Корзина пустая', async ({ page }) => {
+
+    await page.goto('/cart');
+    await expect.soft(page).toHaveScreenshot();
+});
+
+test('Корзина с одним товаром (Шоколад с орехами)', async ({ page }) => {
 
     await page.goto('/catalog');
-    await page.getByTestId('catalog-add-to-cart-button-prod-001').click;
-    await page.getByTestId('catalog-add-to-cart-button-prod-001').click;
-    await page.getByTestId('header-cart-button').click;
-    await expect.soft(page).toHaveScreenshot();
+    await page.getByTestId('catalog-add-to-cart-button-prod-001').click();
+    await page.getByTestId('catalog-add-to-cart-button-prod-001').click();
+    await page.getByTestId('header-cart-button').click();
+    await page.getByTestId('cookie-accept-button').click();
+    await expect.soft(page).toHaveScreenshot({
+        fullPage: true,
+            mask: [
+                // хочется замаскировать и цену на конкретный товар в корзине, но у него нет data-testid :((
+                // наверное нужно делать допустимое отклонение пикселей, или есть другое умное решение?
+                page.getByTestId('cart-total-price'),
+                page.getByTestId('cart-captcha-image')
+            ]
+    });
 });
